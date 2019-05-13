5ee1d0edabbb42a7b70f5b363fe87e6cb44c8aaf
Liam Garrison <liamgarrison@gmail.com>
2019-05-13 11:49:21 +0100

    added reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+  def restaurant_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
   end
 end
