9be3eafde9108a532b8096b623555f39ed43a4dd
Liam Garrison <liamgarrison@gmail.com>
2019-05-13 11:46:21 +0100

    created controllers and wrote restaurant methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
   resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
